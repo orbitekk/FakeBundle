b04575807bd8ab61312adcf49d5367aa5480abcf
Norbert Sroczyński <norbert.sroczynski@eobuwie.com.pl>
2022-08-23 15:17:09 +0200

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,32 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "FakeBundle",
     products: [
-        .executable(name: "fakebundle", targets: ["fakebundle"])
+        .executable(
+            name: "FakeBundle",
+            targets: ["FakeBundle"]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
-        .package(url: "https://github.com/kylef/PathKit.git", from: "1.0.0"),
+        .package(
+            url: "https://github.com/kylef/Commander.git",
+            from: "0.8.0"
+        ),
+        .package(
+            url: "https://github.com/kylef/PathKit.git",
+            from: "1.0.0"
+        )
     ],
     targets: [
-        .target(
-            name: "fakebundle",
-            dependencies: ["Commander", "PathKit"]),
+        .executableTarget(
+            name: "FakeBundle",
+            dependencies: [
+                "Commander",
+                "PathKit"
+            ]
+        )
     ]
 )
